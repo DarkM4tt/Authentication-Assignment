1c8e09c196cf8e46fa17d368c7677893746c8593
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-06-26 02:27:51 +0530

    Created Basic Logging In & Registering Functionality

--- app.js
+++ app.js
@@ -1,97 +1,95 @@
-const express 				= require('express'),
-	  mongoose 				= require('mongoose'),
-	  nodemon				= require('nodemon'),
-	  passport 				= require('passport'),
-	  bodyParser 			= require('body-parser'),
-	  LocalStrategy 		= require('passport-local'),
-	  passportLocalMongoose = require('passport-local-mongoose'),
-	  User					= require('./models/user');
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const LocalStrategy = require('passport-local')
+const passportLocalMongoose = require('passport-local-mongoose')
+const User = require('./models/user')
 
-mongoose.connect('mongodb://localhost/auth_demo_app', {}, function(err) {
-	console.log(err);
-});
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-
-app.use(require('express-session')({
-	secret: 'Mary had a little lamb',
-	resave: false,
-	saveUninitialized: false
-}))
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy(User.authenticate()));
-// reads the session, this is added to our UserSchema through passportLocalMongoose
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// ROUTES
-app.get('/', function(req, res){
-	res.render('home');
+mongoose.connect('mongodb://localhost/auth_demo_app', {}, function (err) {
+  console.log(err)
 })
 
-app.get('/secret', isLoggedIn, function(req, res){
-	res.render('secret');
+const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+
+app.use(
+  require('express-session')({
+    secret: 'Mary had a little lamb',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.get('/', function (req, res) {
+  res.render('welcome')
 })
 
-// AUTH ROUTES
-// displays sign up form
-app.get('/register', function(req, res){
-	res.render('register');
+app.get('/home', isLoggedIn, function (req, res) {
+  res.render('home')
 })
 
-// handles user sign up
-app.post('/register', function(req, res){
-	req.body.username
-	req.body.password
-	User.register(new User({ 
-		username: req.body.username
-	}), req.body.password, function(err, user){
-		if(err){
-			console.log(err);
-			return res.render('register');
-		}
-		passport.authenticate('local')(req, res, function(){
-			res.redirect('/secret');
-		});
-	})
+app.get('/register', function (req, res) {
+  res.render('register')
 })
 
-// LOGIN ROUTES
-// renders log in form
-app.get('/login', function(req, res){
-	res.render('login');
+app.post('/register', function (req, res) {
+  req.body.username
+  req.body.password
+  User.register(
+    new User({
+      username: req.body.username,
+    }),
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err)
+        return res.render('register')
+      }
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/secret')
+      })
+    }
+  )
 })
 
-// login logic
-app.post('/login', passport.authenticate('local', {
-	successRedirect: '/secret',
-	failureRedirect: '/login'
-}), function(req, res){
-	console.log(res);
+app.get('/login', function (req, res) {
+  res.render('login')
 })
 
-// LOGOUT ROUTE
-app.get('/logout', function(req, res){
-	req.logout();
-	res.redirect('/');
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+  }),
+  function (req, res) {
+    console.log(res)
+  }
+)
+
+app.get('/logout', function (req, res) {
+  req.logout()
+  res.redirect('/login')
 })
 
-// check if user is logged in and has access to secret page
-// passed as middleware to secret route
-function isLoggedIn(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	}
-	res.redirect('/login');
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
 }
 
-app.listen(PORT, function(){
-	console.log('server started')
+app.listen(PORT, function () {
+  console.log('server started')
 })
--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 var UserSchema = new mongoose.Schema({
-	username: String,
-	password: String
-});
+  username: String,
+  password: String,
+})
 
 // add methods to UserSchema
 // handles hashing/salting, so no need for bcrypt
-UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose)
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema)
